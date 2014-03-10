c61c0d72e804ce74e0634bcf52b7edc33e10b151
Timothy Griffith <tgriffit@ualberta.ca>
2014-03-10 02:24:02 -0600

    Minor script changes to try better marking of territory

--- Unity/Assets/PartitionTerrain.cs
+++ Unity/Assets/PartitionTerrain.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 
 /*
-	This script divides a rectangular piece of ground into 1 m sections.
-	Bad Assumptions:
-		The surface is a perfect rectangle.
-		The surface has uniform y values.
-		The surface is contiguous.
+This script divides a rectangular piece of ground into 1 m sections.
+Bad Assumptions:
+	The surface is a perfect rectangle.
+	The surface has uniform y values.
+	The surface is contiguous.
 */
 
 public class PartitionTerrain : MonoBehaviour
 {
-	const int TILESIZE = 1;
+	const float TILESIZE = 1.0f;
 	Vector3 corner;
 	int[][] tiles;
 
@@ -19,15 +19,18 @@ public class PartitionTerrain : MonoBehaviour
 	void Start ()
 	{
 		Vector3 center = gameObject.transform.position;
+
+		// This should instead be based on the mesh bounds. This only really works on a simple cube,
+		// since it's size is equal to it's scale.
 		Vector3 size = gameObject.transform.localScale;
 
-		corner = new Vector3 (center.x - size.x / 2, center.y + size.y / 2 + 1, center.z - size.z / 2);
+		corner = new Vector3 (center.x - size.x / 2, center.y + size.y / 2, center.z - size.z / 2);
 
-		int numtiles = (int)size.x / TILESIZE;
+		int numtiles = (int)(size.x / TILESIZE);
 		tiles = new int[numtiles][];
 		for (int i = 0; i < tiles.Length; ++i)
 		{
-				tiles [i] = new int[numtiles];
+			tiles [i] = new int[numtiles];
 		}
 	}
 
@@ -41,16 +44,40 @@ public class PartitionTerrain : MonoBehaviour
 			for (int z = 0; z < tiles[x].Length; ++z)
 			{
 				// Draw 4 lines to represent the borders of the tile
-				Vector3 upperLeft = corner + new Vector3(x * TILESIZE, 0, z * TILESIZE);
-				Vector3 upperRight = corner + new Vector3((x+1) * TILESIZE, 0, z * TILESIZE);
-				Vector3 lowerLeft = corner + new Vector3(x * TILESIZE, 0, (z+1) * TILESIZE);
-				Vector3 lowerRight = corner + new Vector3((x+1) * TILESIZE, 0, (z+1) * TILESIZE);
-				
-				Debug.DrawLine(upperLeft, upperRight, Color.red);
-				Debug.DrawLine(upperRight, lowerRight, Color.red);
-				Debug.DrawLine(lowerRight, lowerLeft, Color.red);
-				Debug.DrawLine(lowerLeft, upperLeft, Color.red);
+				Vector3 upperLeft = corner + new Vector3 (x * TILESIZE, 0, z * TILESIZE);
+				Vector3 upperRight = corner + new Vector3 ((x + 1) * TILESIZE, 0, z * TILESIZE);
+				Vector3 lowerLeft = corner + new Vector3 (x * TILESIZE, 0, (z + 1) * TILESIZE);
+				Vector3 lowerRight = corner + new Vector3 ((x + 1) * TILESIZE, 0, (z + 1) * TILESIZE);
+
+				Debug.DrawLine (upperLeft, upperRight, Color.white);
+				Debug.DrawLine (upperRight, lowerRight, Color.white);
+				Debug.DrawLine (lowerRight, lowerLeft, Color.white);
+				Debug.DrawLine (lowerLeft, upperLeft, Color.white);
+
+				Debug.DrawLine (upperLeft, lowerRight, getTileColor(x, z));
 			}
+		}
+	}
+
+	// Marks the tile debug marker based on the tile's walkablility
+	Color getTileColor (int x, int z)
+	{
+		// Right now, this justs tests for evenness because nothing else is in place.
+		if (x % 2 == 0 && z % 2 == 0)
+		{
+			return Color.red;
+		}
+		else if (x % 2 == 0)
+		{
+			return Color.yellow;
+		}
+		else if (z % 2 == 0)
+		{
+			return Color.cyan;
+		}
+		else
+		{
+			return Color.clear;
 		}
 	}
 }
